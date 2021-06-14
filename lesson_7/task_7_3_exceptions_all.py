2fa89aacd79de1d4b05afc64c419d1a83878e804
SvetlanaTsim <80461844+SvetlanaTsim@users.noreply.github.com>
2021-06-14 20:58:31 +1000

    Update task_7_3_exceptions_all.py
     добавила через shutil.copytree()

--- lesson_7/task_7_3_exceptions_all.py
+++ lesson_7/task_7_3_exceptions_all.py
@@ -31,3 +31,30 @@ for root, dir, files in os.walk('--my_project'):
                     print('Возникла ошибка', e)
                 except Exception as e:
                     print('Глобальная ошибка', e)
+
+# через shutil.copytree()
+#создаем папку "--templates" в папке "--my_project"
+try:
+    os.mkdir('--my_project\\--templates')
+except FileExistsError:
+    print('Файл или папка уже существует')
+except OSError as e:
+    print('Возникла ошибка', e)
+except Exception as e:
+     print('Глобальная ошибка', e)
+
+#ищем шаблоны, копируем папки с шаблонами
+for root, dir, files in os.walk('--my_project'):
+    for file in files:
+        if file.lower().endswith('.html'):
+            file_way = os.path.join(root, file)
+            folder_way = os.path.dirname(file_way)
+            folder_name = os.path.split(folder_way)[-1]
+            if not os.path.exists(f'--my_project\\--templates\\{folder_name}'):
+                try:
+                   shutil.copytree(folder_way, f'--my_project\\--templates\\{folder_name}')
+                except OSError as e:
+                    print('Возникла ошибка', e)
+                except Exception as e:
+                    print('Глобальная ошибка', e)
+                    
